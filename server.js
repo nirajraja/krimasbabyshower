1836b7f413c3b0e425531ede0fc5b4d10fde5382
Niraj Patel <npate015@gmail.com>
2023-05-16 22:27:02 -0400

    Update to heroku server.js

--- server.js
+++ server.js
@@ -5,4 +5,4 @@ app.use(express.static(__dirname + '/dist/heroku-npate-test-appui'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
 '/dist/heroku-npate-test-appui/index.html'));});
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 3000);
