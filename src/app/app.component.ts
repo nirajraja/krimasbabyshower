db86ffc058e562d25ab5c6a25f25ea98eb5ad066
Niraj Patel <npate015@gmail.com>
2023-07-01 20:13:48 -0400

    add auto ping

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DashboardService } from './services/dashboard.service';
+import { interval, takeWhile } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'npate-test-appui';
+
+  constructor(private ds: DashboardService) { };
+
+  ngOnInit(): void {
+    interval(240000).subscribe(() => {
+      this.pingBackEnd();
+    });
+  }
+
+  pingBackEnd() {
+    this.ds.getAllRsvp().subscribe({
+      next: (d) => {
+        console.log('pinged');
+      },
+      error: (d) => {
+        console.log(d);
+      }
+    })
+  }
+
 }
